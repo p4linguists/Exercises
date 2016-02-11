66af7317de043677767ba8e5f48da9bbf5622398
Morteza Rezaei <mrezaeis@gmail.com>
2016-02-11 09:33:27 +0330

    Exercise 2-3

---
+++ 2-3/2-3.py
@@ -0,0 +1,9 @@
+pre=['بد','خوش','با']
+suf=['دار','بان','ساز']
+w=input('Enter Word: ')
+print(pre[0]+'‌'+w)
+print(pre[1]+'‌'+w)
+print(pre[2]+'‌'+w)
+print(w+'‌'+suf[0])
+print(w+'‌'+suf[1])
+print(w+'‌'+suf[2])
