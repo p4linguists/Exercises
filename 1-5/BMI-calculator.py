aad3efb790566680479f3ea0208e52c1496c609b
Morteza Rezaei <mrezaeis@gmail.com>
2016-02-11 08:59:07 +0330

    Exercise 1-5 BMI-calculator

---
+++ 1-5/BMI-calculator.py
@@ -0,0 +1,15 @@
+"""
+BMI= Body Mass Index | شاخص توده بدن
+برای محاسبه بي.ام.آي. وزن به کیلوگرم
+را تقسیم بر مجذور قد به متر می‌کنیم.
+BMI<18.5 کمبود وزن
+18.5<BMI<25 وزن طبیعی
+25<BMI<30 چاقی کم تا متوسط
+30<BMI چاقی مفرط
+"""
+wk=float(input('Enter weight (kg): '))
+hcm=float(input('Enter height (cm): '))
+# hm: convert meters to centimeters
+hm=hcm*0.01
+bmi=wk/hm**2
+print('Your BMI= '+str(round(bmi,2)))
