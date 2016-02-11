53c43da0e3d90d476fb1968525865adb8ffacc8f
Morteza Rezaei <mrezaeis@gmail.com>
2016-02-11 07:59:38 +0330

    Exercise 1-4

---
+++ 1-4/1-4.py
@@ -0,0 +1,13 @@
+pre1='بد'
+pre2='خوش'
+pre3='با'
+suf1='دار'
+suf2='بان'
+suf3='ساز'
+w=input('Enter Word: ')
+print(pre1+' '+w)
+print(pre2+' '+w)
+print(pre3+' '+w)
+print(w+' '+suf1)
+print(w+' '+suf2)
+print(w+' '+suf3)
